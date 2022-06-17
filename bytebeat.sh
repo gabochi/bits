c722279532c368d3cd819d6a4bbcffd1b45ad104
gabochi <gabochi@desktop.vinazza>
2022-06-17 17:29:54 -0300

    todo corriendo

--- bytebeat.sh
+++ bytebeat.sh
@@ -5,8 +5,10 @@ args(){
 	./rpn.sh $expression
 }
 
-#killall -q aplay #>/dev/null 2>&1
+killall -q aplay #>/dev/null 2>&1
 #./hstream.sh $(args $@) | xxd -r -p | aplay >/dev/null 2>&1 &
 c="putchar($(args $@));"
+echo -e "$(head -n 7 program.c) \n ${c} \n $(tail -n 2 program.c)" > temp.c
+gcc -o program temp.c
+./program | aplay >/dev/null 2>&1 &
 
-echo -e "$(head -n 7 program.c) \n ${c} \n $(tail -n 2 program.c)"
---
+++ temp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int t = 0;
+
+  while(1) {
+    t++; 
+ putchar(0); 
+   }
+}
