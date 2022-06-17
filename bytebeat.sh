efe1b84ebb11f9e2cc3f2fd4ad723a6149718047
gabochi <>
2022-06-17 17:51:33 -0300

    nuevo kill

--- bytebeat.sh
+++ bytebeat.sh
@@ -5,8 +5,11 @@ args(){
 	./rpn.sh $expression
 }
 
-killall -q aplay #>/dev/null 2>&1
+#killall -q aplay #>/dev/null 2>&1
 #./hstream.sh $(args $@) | xxd -r -p | aplay >/dev/null 2>&1 &
+pid=$(ps | grep aplay | awk '{print $1}')
+kill -9 $pid
+
 c="putchar($(args $@));"
 echo -e "$(head -n 7 program.c) \n ${c} \n $(tail -n 2 program.c)" > temp.c
 gcc -o program temp.c
